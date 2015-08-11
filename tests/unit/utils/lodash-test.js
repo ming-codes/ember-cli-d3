8278bd33cfbb99197397c34094f883142e333ad9
Ming Liu <vmliu1@gmail.com>
2015-08-10 23:05:55 -0700

    add lodash#scan test

---
+++ tests/unit/utils/lodash-test.js
@@ -0,0 +1,19 @@
+import { module, test } from 'qunit';
+import { scan } from 'ember-d3/utils/lodash';
+
+module('Unit | Utility | lodash');
+
+// Replace this with your real tests.
+test('lodash#scan', function(assert) {
+  var actual, expected;
+
+  actual = scan([ 1, 4, 2, 3, 10 ], ((prev, cur) => prev + cur), 0);
+  expected = [ 1, 5, 7, 10, 20 ];
+
+  assert.deepEqual(actual, expected);
+
+  actual = scan([ 1, 4, 2, 3, 10 ], ((prev, cur) => prev + cur));
+  expected = [ undefined, 5, 7, 10, 20 ];
+
+  assert.deepEqual(actual, expected);
+});
