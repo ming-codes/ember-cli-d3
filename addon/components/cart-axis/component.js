404356d9de6a7573e1466d5d751fbfa11b8c3f33
Ming Liu <vmliu1@gmail.com>
2015-08-16 22:06:46 -0700

    add more options to axis

--- addon/components/cart-axis/component.js
+++ addon/components/cart-axis/component.js
@@ -3,24 +3,24 @@ import layout from './template';
 
 import EmberD3 from '../../mixins/d3-support';
 
-//import { join, translateX } from '../../utils/d3';
+import { assign } from '../../utils/d3';
 
 export default Ember.Component.extend(EmberD3, {
   layout,
 
-  tickSize: 6,
   transform: null,
 
-  axis: Ember.computed('scale', 'tickSize', {
-    get() {
-      var tickSize = this.get('tickSize');
-      var scale = this.get('scale');
-      var orient = this.get('orient');
+  tickSize: 6,
+  tickFormat: null,
+  tickPadding: null,
 
-      return scale && d3.svg.axis()
-        .scale(scale)
-        .orient(orient)
-        .tickSize(tickSize);
+  scale: null,
+
+  axis: Ember.computed('scale', 'orient', 'tickSize', 'tickFormat', 'tickPadding', {
+    get() {
+      var props = this.getProperties('scale', 'orient', 'tickSize', 'tickFormat', 'tickPadding');
+
+      return props.scale && assign(d3.svg.axis(), props);
     }
   }).readOnly(),
 
