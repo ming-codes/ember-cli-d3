f7b99dd4de09fa578bfb63560eb21b460dad79f3
Ming Liu <vmliu1@gmail.com>
2015-08-23 00:30:14 -0700

    implements transition helper test

--- tests/unit/helpers/transition-test.js
+++ tests/unit/helpers/transition-test.js
@@ -1,10 +1,20 @@
+import SelectionProxy from 'ember-cli-d3/components/data-visual/d3-selection-proxy';
 import { transition } from '../../../helpers/transition';
 import { module, test } from 'qunit';
 
 module('Unit | Helper | transition');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  //var result = transition(42);
-  //assert.ok(result);
+test('empty selection should not throw', function(assert) {
+  assert.expect(0);
+
+  transition([], {});
+});
+
+test('apply d3 transition options when set', function(assert) {
+  var delay = Math.floor(Math.random() * 1000);
+  var duration = Math.floor(Math.random() * 1000);
+  var sel = transition([ SelectionProxy.create() ], { delay, duration }).get('selection');
+
+  assert.equal(delay, sel.delay());
+  assert.equal(duration, sel.duration());
 });
