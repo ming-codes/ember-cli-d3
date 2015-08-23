197af85ebcc533223b613d5562065fe4b84479e5
Ming Liu <vmliu1@gmail.com>
2015-08-22 17:17:47 -0700

    repeated calls in ext should be fine

--- addon/ext/d3.js
+++ addon/ext/d3.js
@@ -1,33 +1,59 @@
 
 import { wrap, slice } from 'ember-cli-d3/utils/lodash';
 
+export function id() {
+  d3.selection.prototype.id = d3.selection.prototype.id || function (newId) {
+    if (arguments.length) {
+      this.node().id = newId;
+
+      return this;
+    }
+
+    return this.node().id;
+  };
+}
+
 export function type() {
   d3.selection.prototype.isSelection = true;
   d3.transition.prototype.isTransition = true;
 }
 
 export function timer() {
+  if (d3.timer.__extended__) { return; }
+
+  var sem = 0;
+
   var timerFlush = d3.timer.flush;
+  var wait = true;
 
   d3.timer = wrap(d3.timer, function() {
     var args = slice.call(arguments);
     var fn = args.shift();
     var callbacks = args[0];
 
-    args[0] = function () {
-      var context = this;
-      var callbackArgs = slice.call(arguments);
-      var ret;
+    if (wait) {
+      sem++;
+    }
 
-      Ember.run(function () {
-        ret = callbacks.apply(context, callbackArgs);
-      });
+    wait = false;
+
+    args[0] = wrap(args[0], function (fn, ...args) {
+      var ret = fn.apply(this, args);
+
+      wait = true;
+
+      if (ret) {
+        sem--;
+      }
 
       return ret;
-    };
+    });
 
     fn.apply(this, args);
   });
 
   d3.timer.flush = timerFlush;
+  d3.timer.__extended__ = 1;
+
+  Ember.Test.registerWaiter(() => wait);
 }
