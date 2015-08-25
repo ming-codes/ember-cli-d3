e0eee22d99e857676482b7c2abac7ef954adf479
Ming Liu <vmliu1@gmail.com>
2015-08-24 19:41:18 -0700

    normalizeEntityName is required

--- blueprints/ember-cli-d3/index.js
+++ blueprints/ember-cli-d3/index.js
@@ -3,6 +3,8 @@
 module.exports = {
   description: 'Add D3 bower module to project',
 
+  normalizeEntityName: function() { },
+
   afterInstall: function(options) {
     return this.addBowerPackageToProject('d3');
   }
