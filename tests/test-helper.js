fdc6462ce3523a2bb18c167360a171fd1797e17b
Ming Liu <vmliu1@gmail.com>
2015-09-02 21:32:42 -0700

    always register helper in test

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,4 +1,7 @@
 import resolver from './helpers/resolver';
 import { setResolver } from 'ember-qunit';
+import { initialize } from 'dummy/initializers/ember-cli-auto-register-helpers';
+
+initialize();
 
 setResolver(resolver);
