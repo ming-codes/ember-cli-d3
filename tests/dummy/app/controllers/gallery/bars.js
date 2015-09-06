6f7e1755920a0df4402a6e0969d99a6071ba288d
Ming Liu <vmliu1@gmail.com>
2015-09-06 16:50:29 -0700

    lets put the charts back in

--- tests/dummy/app/controllers/gallery/bars.js
+++ tests/dummy/app/controllers/gallery/bars.js
@@ -5,24 +5,24 @@ import DimensionalDataModel from 'dummy/utils/model/dimensional';
 import { computed } from 'ember-cli-d3/utils/version';
 
 export default Ember.Controller.extend({
-  model: null
+  visual: null,
 
-  //app: Ember.inject.controller('application'),
+  app: Ember.inject.controller('application'),
 
-  //dataSource: Ember.inject.service('dimensional-data-source'),
+  dataSource: Ember.inject.service('dimensional-data-source'),
 
-  //isStacked: Ember.computed.equal('app.currentRouteName', 'cart.bars.stacked'),
-  //isGrouped: Ember.computed.equal('app.currentRouteName', 'cart.bars.grouped'),
-  //isWaterfall: Ember.computed.equal('app.currentRouteName', 'cart.bars.waterfall'),
+  isStacked: Ember.computed.equal('app.currentRouteName', 'gallery.bars.stacked'),
+  isGrouped: Ember.computed.equal('app.currentRouteName', 'gallery.bars.grouped'),
+  isWaterfall: Ember.computed.equal('app.currentRouteName', 'gallery.bars.waterfall'),
 
-  //data: Ember.computed.alias('dataSource.data'),
-  //series: [ 'dogs', 'cats' ],
-  //key: 'state',
+  data: Ember.computed.alias('dataSource.data'),
+  series: [ 'dogs', 'cats' ],
+  key: 'state',
 
-  //dim: computed('data', 'series', 'key', {
-  //  get() {
-  //    return DimensionalDataModel.create(this.getProperties('data', 'series', 'key'));
-  //  }
-  //}),
+  dimensionalData: computed('data', 'series', 'key', {
+    get() {
+      return DimensionalDataModel.create(this.getProperties('data', 'series', 'key'));
+    }
+  }),
 
 });
