8a3d416511e1c09d5fad82590d1dd5121a8bfefc
Ming Liu <vmliu1@gmail.com>
2015-09-06 18:43:29 -0700

    generate thumbnail from model

--- tests/dummy/app/models/visual.js
+++ tests/dummy/app/models/visual.js
@@ -5,7 +5,9 @@ const VisualModel = DS.Model.extend({
   alias: DS.attr('array'),
   route: DS.attr('string'),
   description: DS.attr('string'),
-  variations: DS.hasMany('visual')
+  variations: DS.hasMany('visual'),
+  component: DS.attr('string'),
+  modelType: DS.attr('string')
 });
 
 VisualModel.reopenClass({
@@ -15,6 +17,8 @@ VisualModel.reopenClass({
       name: 'Grouped Bars',
       alias: [],
       route: 'gallery.bars.grouped',
+      component: 'cart-grouped-bars',
+      modelType: 'dimensional',
       description: 'This is a grouped bar chart',
       variations: [ 1 ]
     },
@@ -23,6 +27,8 @@ VisualModel.reopenClass({
       name: 'Stacked Bars',
       alias: [],
       route: 'gallery.bars.stacked',
+      component: 'cart-stacked-bars',
+      modelType: 'dimensional',
       description: 'This is a stacked bar chart',
       variations: [ 0 ]
     },
