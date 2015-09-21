ab897287c49fe7ccd855e138724cddb4b9fcb7fa
Ming Liu <vmliu1@gmail.com>
2015-09-20 22:35:39 -0700

    use markdown component to prepare for toc

---
+++ tests/dummy/app/components/markdown-document.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+import layout from '../templates/components/markdown-document';
+
+import { slice } from 'ember-cli-d3/utils/lodash';
+
+export default Ember.Component.extend({
+  layout,
+
+  classNames: [ 'markdown-body' ],
+
+  content: null,
+
+  didRender() {
+    var headers = slice.call(this.$('h1, h2, h3, h4, h5'))
+
+    var toc = headers.map(header => {
+    });
+
+    headers.forEach(header => {
+      header.id = header.textContent.toLowerCase().replace(/ /g, '-');
+    });
+
+    this.set('toc', '');
+  }
+});
