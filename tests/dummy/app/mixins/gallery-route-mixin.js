afa80f58910db078bbc4e601a65820b73fe1bb56
Ming Liu <vmliu1@gmail.com>
2015-09-26 09:58:54 -0700

    refactor model loading as mixin

---
+++ tests/dummy/app/mixins/gallery-route-mixin.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Mixin.create({
+  model() {
+    return this.store.findRecord('visual', this.routeName);
+  },
+  setupController(controller, model) {
+    this.controllerFor(this.routeName.split('.').slice(0, 1).join('/')).setProperties({ model });
+  }
+});
