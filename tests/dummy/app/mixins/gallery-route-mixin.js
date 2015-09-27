f7ccd92235a81ed8342ba9300866e2f82d6b35e9
Ming Liu <vmliu1@gmail.com>
2015-09-26 18:25:55 -0700

    fix bug in route mixin

--- tests/dummy/app/mixins/gallery-route-mixin.js
+++ tests/dummy/app/mixins/gallery-route-mixin.js
@@ -5,6 +5,6 @@ export default Ember.Mixin.create({
     return this.store.findRecord('visual', this.routeName);
   },
   setupController(controller, model) {
-    this.controllerFor(this.routeName.split('.').slice(0, 1).join('/')).setProperties({ model });
+    this.controllerFor(this.routeName.split('.').slice(0, 2).join('/')).setProperties({ model });
   }
 });
