cc2ffa2666b4a34d0f7427d52fd590391045bd3a
Ming Liu <vmliu1@gmail.com>
2016-01-11 10:00:42 -0800

    correct yield support content

--- addon/mixins/yield-support.js
+++ addon/mixins/yield-support.js
@@ -6,21 +6,31 @@ export default Ember.Mixin.create({
 
   yieldProperties: null,
 
-  content: Ember.computed('parentView', 'context', function () {
-	  return Ember.ObjectProxy.create({
-		  content: this.nearestWithProperty('hasYieldSupport') || this.get('_parentView.context')
-	  });
+  content: Ember.computed('parentView', 'context', 'yieldProperties', function () {
+    let properties = this.get('yieldProperties').reduce((accum, prop) => {
+      accum[prop] = null;
+
+      return accum;
+    }, {});
+    let content = this.nearestWithProperty('hasYieldSupport') || this;
+
+	  return Ember.ObjectProxy.create(Ember.merge(properties, {
+      content: content.get('origContext') || content.get('_parentView.context')
+	  }));
   }).readOnly(),
 
   layout: Ember.computed(function () {
-    return Ember.Handlebars.compile('{{view template=template context=content tagName=""}}');
+    return Ember.Handlebars.compile('{{view template=template context=content tagName="" isVirtual=true}}');
   }).readOnly(),
 
   init() {
     this._super(...arguments);
 
     this.getWithDefault('yieldProperties', []).forEach(prop => {
-      this[`${prop}Binding`] = Ember.Binding.from(prop).to(`content.${prop}`).connect(this);
+      this[`${prop}Binding`] = Ember.Binding
+        .from(prop)
+        .to(`content.${prop}`)
+        .connect(this);
     });
   },
 
