a9294d6b4b4679f13e6cf04cdda8fdec30495e84
Ming Liu <vmliu1@gmail.com>
2016-03-20 12:06:07 -0700

    migrate from ic-ajax

--- tests/dummy/app/services/geospatial-data-source.js
+++ tests/dummy/app/services/geospatial-data-source.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
-import { request } from 'ic-ajax';
 
 export default Ember.Service.extend(Ember.PromiseProxyMixin, {
   data: Ember.computed.alias('content'),
+  ajax: Ember.inject.service(),
 
   init() {
     this._super(...arguments);
 
-    this.set('promise', request('us.json'));
+    this.set('promise', this.get('ajax').request('us.json'));
   }
 });
