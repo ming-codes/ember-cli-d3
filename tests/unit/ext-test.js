28130b2eb9ab25e4b04d8e57a53b01aa26ae90b7
Ming Liu <vmliu1@gmail.com>
2016-03-20 14:27:32 -0700

    test case to ensure style attr basic usage still correct

---
+++ tests/unit/ext-test.js
@@ -0,0 +1,91 @@
+
+import Ember from 'ember';
+
+import { module, test } from 'qunit';
+
+import d3 from 'd3';
+
+module('Unit | Extensions', {
+  beforeEach() {
+    this.rect = d3.select('#ember-testing')
+      .append('svg')
+      .append('rect');
+  },
+
+  afterEach() {
+    this.rect.remove();
+    this.rect = null;
+  }
+});
+
+test('d3.selection.prototype#style string setter', function (assert) {
+  let expected = 'rgb(0, 0, 0)';
+  let actual = this.rect
+    .style('fill', expected)
+    .style('fill');
+
+  assert.equal(actual, expected);
+});
+
+test('d3.selection.prototype#style hash setter', function (assert) {
+  let expected = 'rgb(0, 0, 0)';
+  let actual = this.rect
+    .style({ fill: expected })
+    .style('fill');
+
+  assert.equal(actual, expected);
+});
+
+test('d3.transition.prototype#style string setter', function (assert) {
+  return new Ember.RSVP.Promise(resolve => {
+    let expected = 'rgb(255, 255, 255)';
+
+    this.rect
+      .style('fill', 'rgb(0, 0, 0)')
+      .transition()
+      .style('fill', expected)
+      .each('end', function () {
+        let actual = d3.select(this).style('fill');
+
+        assert.equal(actual, expected);
+
+        resolve();
+      });
+  });
+});
+
+test('d3.transition.prototype#style hash setter', function (assert) {
+  return new Ember.RSVP.Promise(resolve => {
+    let expected = 'rgb(255, 255, 255)';
+
+    this.rect
+      .style({ fill: 'rgb(0, 0, 0)' })
+      .transition()
+      .style({ fill: expected })
+      .each('end', function () {
+        let actual = d3.select(this).style('fill');
+
+        assert.equal(actual, expected);
+
+        resolve();
+      });
+  });
+});
+
+test('d3.selection.prototype#attr string setter', function (assert) {
+  let expected = 'classy';
+  let actual = this.rect
+    .attr('class', expected)
+    .attr('class');
+
+  assert.equal(actual, expected);
+});
+
+test('d3.selection.prototype#attr hash setter', function (assert) {
+  let expected = 'classy';
+  let actual = this.rect
+    .attr({ 'class': expected })
+    .attr('class');
+
+  assert.equal(actual, expected);
+});
