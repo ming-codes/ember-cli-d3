76f626469cda1bd4dfe133705991cb8604920177
Andreas Monitzer <andy@monitzer.com>
2016-03-29 15:44:03 +0200

    Implemented selection.

--- addon/system/html-proxy.js
+++ addon/system/html-proxy.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
+import d3 from 'd3';
 
 export default Ember.Object.extend({
+  select: Ember.computed('element', function() {
+    return d3.select(this.get('element'));
+  }).readOnly(),
+  
   call(component) {
     var element = this.get('element');
 
